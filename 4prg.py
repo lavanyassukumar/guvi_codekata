34554629ff338e08a085fd9859952820ac30dc6c
lavanyassukumar <36658885+lavanyassukumar@users.noreply.github.com>
2018-02-20 15:46:53 +0530

    Add files via upload

---
+++ 4prg.py
@@ -0,0 +1,6 @@
+mystring=raw_input()
+string=('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+if mystring in string:
+	print("it is an alphabet")
+else:
+	print("it is not an alphabet")
