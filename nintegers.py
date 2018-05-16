64e6eca049064c751eedeaf46d07665168ec88d0
lavanyassukumar <36658885+lavanyassukumar@users.noreply.github.com>
2018-05-16 14:36:19 +0530

    Add files via upload

---
+++ nintegers.py
@@ -0,0 +1,26 @@
+num=input("enter the no:\n")
+
+count=0
+
+sum=1
+
+if num<0:
+
+    num=num+(-num)+(-num)
+    while num>0:
+
+        count=count+sum
+ 
+        num=num//10
+ 
+    print "the no. of integers is",count	
+
+else:	
+    
+    while num>0:
+   
+        count=count+sum
+     
+        num=num//10
+  
+    print "the no. of integers is",count
